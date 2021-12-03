81483a04567ad626c5b61637308789a505b64b35
mohammedelkouche <mohammed0elkouche@gmail.com>
2021-12-03 10:17:55 +0100

    addFile

---
+++ GameRandom.js
@@ -0,0 +1,53 @@
+// var myinput = document.getElementById("input");
+// var myinput = myinput.value ;
+var btn =document.getElementById("Guess");
+var Random = Math.floor(Math.random()*100+1);
+var attempts =10 ;
+btn.addEventListener('click',function()
+    {
+        var myinput = document.getElementById("input").value;
+        if(Number.isInteger(Number(myinput))==true)
+        {
+            attempts-- ;
+
+            if(myinput == Random && attempts>8){
+                
+                document.getElementById("comparison").innerHTML=("Well done, you are a Genius"); 
+                document.getElementById("Parg-Attempts").style.display="none";
+
+            }
+            else if(myinput == Random && attempts<=8){
+                var turn =10-attempts;
+                document.getElementById("comparison").style.display="none"; 
+                document.getElementById("Parg-Attempts").innerHTML=("Congratulations, you win after "+ turn + " attempts");
+
+            }
+
+            else if(myinput > Random){
+                
+                document.getElementById("comparison").innerHTML=("the number guessed is bigger than the secret number");
+                document.getElementById("Parg-Attempts").innerHTML=("You have "+ attempts +" attempts left");
+            }
+            else if(attempts<=0){
+                document.getElementById("comparison").style.display="none"; 
+                document.getElementById("Parg-Attempts").innerHTML=("you have exceeded the number of allowed attempts");
+                // break ;
+            }
+            else if(myinput<=0 || myinput>100){
+                document.getElementById("comparison").style.display="none"; 
+                document.getElementById("Parg-Attempts").innerHTML=("Please enter a number within 1 - 100");
+            }
+            else{
+
+                document.getElementById("comparison").innerHTML=("the number guessed is smaller than the secret number");
+                document.getElementById("Parg-Attempts").innerHTML=("You have "+attempts +" attempts left");
+            }
+
+        }
+        else {
+
+            document.getElementById("comparison").innerHTML=("Please Enter Any  Number !!");
+    
+        }
+    }
+)
